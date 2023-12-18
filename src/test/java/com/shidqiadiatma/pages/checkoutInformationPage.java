8cb58ef39c58f5902135fff9711b4d6714bcdb04
hipstertester <shidqiadiatma12@gmail.com>
2023-12-18 11:20:15 +0700

    added pageObject checkout information

--- src/test/java/com/shidqiadiatma/pages/checkoutInformationPage.java
+++ src/test/java/com/shidqiadiatma/pages/checkoutInformationPage.java
@@ -16,9 +16,23 @@ public class checkoutInformationPage {
 
     public checkoutInformationPage(AppiumDriver driver) {
         this.appiumDriver = driver;
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         PageFactory.initElements(driver, this);
     }
+
+    @FindBy(xpath = "//android.view.ViewGroup[@content-desc='test-Menu']/android.view.ViewGroup/android.widget.ImageView")
+    private WebElement burgerNav_icon;
+
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.ImageView[2]")
+    private WebElement swagLabs_icon;
+
+    @FindBy(xpath = "//android.view.ViewGroup[@content-desc='test-Cart']/android.view.ViewGroup/android.widget.ImageView")
+    private WebElement cart_icon;
+    @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]")
+    private WebElement top_greyBox;
+
+    @FindBy(xpath = "//android.widget.ScrollView[@content-desc='test-Checkout: Your Info']/android.view.ViewGroup/android.view.ViewGroup[8]")
+    private WebElement bottom_greyBox;
 
     @FindBy(xpath = "//android.widget.EditText[@content-desc='test-First Name']")
     private WebElement input_firstName;
@@ -29,16 +43,33 @@ public class checkoutInformationPage {
     @FindBy(xpath = "//android.widget.EditText[@content-desc='test-Zip/Postal Code']")
     private WebElement input_postalCode;
 
+    @FindBy(xpath = "//android.view.ViewGroup[@content-desc='test-CANCEL']")
+    private WebElement cancel_button;
+
+
     @FindBy(xpath = "//android.view.ViewGroup[@content-desc='test-CONTINUE']")
     private WebElement continue_button;
 
 
 
-    public void fill_checkoutInformation() {
-        keyword.waitUntilElementIsVisible(input_firstName);
-        keyword.inputText(input_firstName, "samsul");
-        keyword.inputText(input_lastName, "ipong");
-        keyword.inputText(input_postalCode, "33783");
+    public void verify_Component_OnCheckoutInformation () {
+        keyword.verifyElementExist(burgerNav_icon);
+        keyword.verifyElementExist(swagLabs_icon);
+        keyword.verifyElementExist(cart_icon);
+        keyword.verifyElementExist(top_greyBox);
+        keyword.verifyElementExist(bottom_greyBox);
+        keyword.verifyElementExist(input_firstName);
+        keyword.verifyElementExist(input_lastName);
+        keyword.verifyElementExist(input_postalCode);
+        keyword.verifyElementExist(continue_button);
+        keyword.verifyElementExist(cancel_button);
+
+
+    }
+    public void fill_checkoutInformation(String firstName, String lastName, String postalCode) {
+        keyword.inputText(input_firstName, firstName);
+        keyword.inputText(input_lastName, lastName);
+        keyword.inputText(input_postalCode, postalCode);
         keyword.tapElement(continue_button);
         keyword.waitFor(3);
 
