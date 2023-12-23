490d35f42f00fe91325b105cf8c1655a859f60ab
Shidqi Adiatma <adiatma944@gmail.com>
2023-12-23 16:43:24 +0700

    refactor checkoutStep

--- src/test/java/com/shidqiadiatma/steps/checkoutStep.java
+++ src/test/java/com/shidqiadiatma/steps/checkoutStep.java
@@ -1,47 +1,52 @@
 package com.shidqiadiatma.steps;
 
 import com.shidqiadiatma.factories.driverManager;
-import com.shidqiadiatma.pages.*;
+import com.shidqiadiatma.helpers.randomData;
 import io.appium.java_client.android.AndroidDriver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import com.shidqiadiatma.helpers.randomData;
+import com.shidqiadiatma.pages.*;
 
 public class checkoutStep {
 
-    private final AndroidDriver androidDriver;
+    private final HomePage homePage;
+    private final YourCartPage yourCartPage;
+    private final CheckoutInformationPage checkoutInformationPage;
+    private final CheckoutOverviewPage checkoutOverviewPage;
+    private final CheckoutCompletePage checkoutCompletePage;
 
     public checkoutStep() {
-        androidDriver = driverManager.getInstance().getDriver();
+        AndroidDriver androidDriver = driverManager.getInstance().getDriver();
+        homePage = new HomePage(androidDriver);
+        yourCartPage = new YourCartPage(androidDriver);
+        checkoutInformationPage = new CheckoutInformationPage(androidDriver);
+        checkoutOverviewPage = new CheckoutOverviewPage(androidDriver);
+        checkoutCompletePage = new CheckoutCompletePage(androidDriver);
     }
 
     @When("user tap Add To Cart button on first product")
     public void userTapAddToCartButtonOnFirstProduct() {
-        homePage homePage = new homePage(androidDriver);
         homePage.add_firstProduct_to_cart();
     }
+
     @And("user tap Add To Cart button on second product")
     public void userTapAddToCartButtonOnSecondProduct() {
-        homePage homePage = new homePage(androidDriver);
         homePage.add_secondProduct_to_cart();
     }
 
     @And("user tap Cart icon")
     public void userTapCartIcon() {
-        homePage homePage = new homePage(androidDriver);
         homePage.tap_cartIcon();
     }
 
     @And("user tap Checkout button on Your Cart page")
     public void userTapCheckoutButton() {
-        yourCartPage yourCartPage = new yourCartPage(androidDriver);
         yourCartPage.tap_checkoutButton();
     }
 
     @And("user fill in Checkout Information")
     public void userFillInCheckoutInformation() {
-        checkoutInformationPage checkoutInformationPage = new checkoutInformationPage(androidDriver);
         checkoutInformationPage.verify_Component_OnCheckoutInformation();
         checkoutInformationPage.fill_checkoutInformation(
                 randomData.getFirstName(),
@@ -52,15 +57,11 @@ public class checkoutStep {
 
     @And("user tap Finish button on Checkout Overview page")
     public void userTapFinishButtonOnCheckoutOverviewPage() {
-        checkoutOverviewPage checkoutOverviewPage = new checkoutOverviewPage(androidDriver);
         checkoutOverviewPage.tap_finishButton();
     }
 
     @Then("user verify success checkout")
     public void userVerifySuccessCheckout() {
-        checkoutCompletePage checkoutCompletePage = new checkoutCompletePage(androidDriver);
         checkoutCompletePage.verify_component_OnCheckoutComplete();
-
     }
-
 }
