61db68cc7bc83d5fa0a41520cbdf0fdf30da9e94
Shidqi Adiatma <adiatma944@gmail.com>
2023-12-23 16:44:13 +0700

    refactor commonStep

--- src/test/java/com/shidqiadiatma/steps/commonStep.java
+++ src/test/java/com/shidqiadiatma/steps/commonStep.java
@@ -1,32 +1,34 @@
 package com.shidqiadiatma.steps;
 
-import com.shidqiadiatma.factories.driverManager;
-import com.shidqiadiatma.helpers.keyword;
-import com.shidqiadiatma.pages.loginPage;
 import io.appium.java_client.android.AndroidDriver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import com.shidqiadiatma.factories.driverManager;
+import com.shidqiadiatma.helpers.keyword;
+import com.shidqiadiatma.pages.HomePage;
+import com.shidqiadiatma.pages.LoginPage;
 
 public class commonStep {
 
-    private final AndroidDriver androidDriver;
+    private final LoginPage loginPage;
+    private final HomePage homePage;
 
     public commonStep() {
-        androidDriver = driverManager.getInstance().getDriver();
+        AndroidDriver androidDriver = driverManager.getInstance().getDriver();
+        loginPage = new LoginPage(androidDriver);
+        homePage = new HomePage(androidDriver);
     }
 
     @Given("user is already on home page")
     public void userIsAlreadyOnHomePage() {
-        loginPage loginPage = new loginPage(androidDriver);
         loginPage.verify_Component_OnLoginPage();
-        loginPage.tap_Autofill();
-        loginPage.verify_usernameIsFilledIn();
+        loginPage.doLogin("standard_user", "secret_sauce");
         loginPage.tap_loginButton();
+        homePage.verify_Component_OnHomePage("PRODUCTS");
     }
 
     @Given("user open SwagLabs app")
     public void userOpenSwagLabsApp() {
-        loginPage loginPage = new loginPage(androidDriver);
         loginPage.verify_Component_OnLoginPage();
     }
 
