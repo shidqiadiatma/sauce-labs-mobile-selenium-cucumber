6128c7b99169592306ba779b0d92d0818c5171fe
Shidqi Adiatma <adiatma944@gmail.com>
2023-12-23 16:45:34 +0700

    refactor sortingStep

--- src/test/java/com/shidqiadiatma/steps/sortingStep.java
+++ src/test/java/com/shidqiadiatma/steps/sortingStep.java
@@ -1,32 +1,30 @@
 package com.shidqiadiatma.steps;
 
 import com.shidqiadiatma.factories.driverManager;
-import com.shidqiadiatma.pages.homePage;
+import com.shidqiadiatma.pages.HomePage;
 import io.appium.java_client.android.AndroidDriver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.testng.Assert;
 
 public class sortingStep {
+
     private final AndroidDriver androidDriver;
+    private final HomePage homePage;
 
     public sortingStep() {
         androidDriver = driverManager.getInstance().getDriver();
+        homePage = new HomePage(androidDriver);
     }
-
 
     @When("user select filter price high to low")
     public void userSelectFilter() {
-        homePage homePage = new homePage(androidDriver);
         homePage.tap_changeView_icon();
         homePage.tap_FilterIcon();
         homePage.tap_HighToLow();
     }
 
     @Then("compare first product price with second product price")
-    public void compareProductPrice(){
-        new homePage(androidDriver);
-        homePage.comparePrices(androidDriver);
-
+    public void compareProductPrice() {
+        HomePage.comparePrices(androidDriver);
     }
 }
