5e72f244338b6b5faf7eb37a36a717abb384097f
Shidqi Adiatma <adiatma944@gmail.com>
2023-12-23 16:50:09 +0700

    add component

--- src/test/java/com/shidqiadiatma/pages/CheckoutCompletePage.java
+++ src/test/java/com/shidqiadiatma/pages/CheckoutCompletePage.java
@@ -8,21 +8,19 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.concurrent.TimeUnit;
 
-public class checkoutCompletePage {
-
+public class CheckoutCompletePage {
     protected AppiumDriver appiumDriver;
-
-    public checkoutCompletePage (AppiumDriver driver) {
+    public CheckoutCompletePage(AppiumDriver driver) {
         this.appiumDriver = driver;
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "//android.widget.ScrollView[@content-desc='test-CHECKOUT: COMPLETE!']/android.view.ViewGroup/android.widget.TextView[1]")
-    private WebElement title_checkoutComplete;
+    private WebElement txt_ThankYouForYouOrder;
 
     @FindBy(xpath = "//android.widget.ScrollView[@content-desc='test-CHECKOUT: COMPLETE!']/android.view.ViewGroup/android.widget.TextView[2]")
-    private WebElement description_checkoutComplete;
+    private WebElement desc_checkoutComplete;
 
     @FindBy(xpath = "//android.widget.ScrollView[@content-desc='test-CHECKOUT: COMPLETE!']/android.view.ViewGroup/android.widget.ImageView")
     private WebElement image_onCheckoutComplete;
@@ -31,18 +29,10 @@ public class checkoutCompletePage {
     private WebElement backHome_button;
 
     public void verify_component_OnCheckoutComplete () {
-        keyword.verifyElementExist(title_checkoutComplete);
-        keyword.verifyElementExist(description_checkoutComplete);
+        keyword.verifyElementExist(txt_ThankYouForYouOrder);
+        keyword.verifyElementExist(desc_checkoutComplete);
         keyword.verifyElementExist(image_onCheckoutComplete);
         keyword.verifyElementExist(backHome_button);
     }
-
-
-
-
-
-
-
-
 
 }
